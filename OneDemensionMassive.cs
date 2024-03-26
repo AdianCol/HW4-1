0f6bc8ddf6f4486830eea7c38461a68fa65c176c
AdianCol <artem.baykin27@gmail.com>
2024-03-26 23:51:18 +0300

    Alpha

---
+++ OneDemensionMassive.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+
+public sealed class OneDemensionMassive<T>
+{
+    private T[] massive;
+    private Random r = new Random();
+    private int len = 10;
+
+    public OneDemensionMassive(Func<T[], bool, T[]> action) 
+    {
+        CreateMassive(action);
+    }
+
+    public void CreateMassive(Func<T[], bool, T[]> action)
+    {
+        Console.WriteLine("How to fill the array");
+        bool choice = bool.Parse(Console.ReadLine());
+        if (choice)
+        {
+            massive = new T[int.Parse(Console.ReadLine())];
+        }
+        else
+        {
+            massive = new T[len];
+        }
+        massive = action(massive, choice);
+    }
+
+    public void AddElement(Func<T[], T[]> action)
+    {
+        massive = action(massive);
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Massive");
+        foreach (T i in massive)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void DeleteElement(Func<T[], int, T[]> action)
+    {
+        Console.WriteLine("Write the index of element you will remove");
+        int ind = int.Parse(Console.ReadLine());
+        massive = action(massive, ind);
+    }
+
+    public void Sorting()
+    {
+        Array.Sort(massive);
+    }
+
+    public bool EvenIfOne(Func<T, bool> action)
+    {
+        foreach (T i in massive)
+        {
+            if (action(i))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool IfAll(Func<T, bool> action)
+    {
+        foreach (T i in massive)
+        {
+            if (!action(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void CountWithIf(Func<T, bool> action)
+    {
+        int c = 0;
+        foreach(T i in massive)
+        {
+            if(action(i)){
+                c++;
+            }
+        }
+        Console.WriteLine(c);
+    }
+
+}
---
+++ Prog.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+class Prog{
+    static void Main()
+    {
+        Random r = new Random();
+        OneDemensionMassive<int> intArray = new OneDemensionMassive<int>((m, b) =>
+        {
+            int[] mass = new int[m.Length];
+            if (b)
+            {
+                for(int i = 0; i < mass.Length; i++){
+                    mass[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            else
+            {
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    mass[i] = r.Next(-1000, 1000);
+                }
+            }
+            return mass;
+        });
+        intArray.Print();
+        OneDemensionMassive<double> doubleArray = new OneDemensionMassive<double>((m, b) =>
+        {
+            double[] mass = new double[m.Length];
+            if (b)
+            {
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    mass[i] = double.Parse(Console.ReadLine());
+                }
+            }
+            else
+            {
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    mass[i] = r.Next(-1000, 1000)/r.Next(-10, 10);
+                }
+            }
+            return mass;
+        });
+        doubleArray.Print();
+        intArray.AddElement((m)=>
+        {
+            int[] mass = new int[m.Length+1];
+            for (int i = 0; i < m.Length; i++)
+            {
+                mass[i] = m[i];
+            }
+            mass[m.Length] = r.Next(-1000, 1000);
+            return mass;
+        });
+        intArray.Print();
+        doubleArray.AddElement((m) =>
+        {
+            double[] mass = new double[m.Length + 1];
+            for (int i = 0; i < m.Length; i++)
+            {
+                mass[i] = m[i];
+            }
+            mass[m.Length] = r.Next(-1000, 1000) / r.Next(-10, 10);
+            return mass;
+        });
+        doubleArray.Print();
+        intArray.DeleteElement((m,i)=>
+        {
+            int[] mass = new int[m.Length-1];
+            for (int t = 0; t<m.Length; t++)
+            {
+                if(t<i)
+                {
+                    mass[t] = m[t];
+                }
+                else if(t>i)
+                {
+                    mass[t-1] = m[t];
+                }
+            }
+            return mass;
+        });
+        intArray.Print();
+        doubleArray.DeleteElement((m, i) =>
+        {
+            double[] mass = new double[m.Length - 1];
+            for (int t = 0; t < m.Length; t++)
+            {
+                if (t < i)
+                {
+                    mass[t] = m[t];
+                }
+                else if (t > i)
+                {
+                    mass[t - 1] = m[t];
+                }
+            }
+            return mass;
+        });
+        doubleArray.Print();
+        intArray.Sorting();
+        intArray.Print();
+        doubleArray.Sorting();
+        doubleArray.Print();
+        intArray.CountWithIf((i)=>
+        {
+            if(i>50){
+                return true;
+            }
+            else{
+                return false;
+            }
+        });
+        doubleArray.CountWithIf((i) =>
+        {
+            if (i < -60.7)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        });
+        Console.WriteLine(intArray.EvenIfOne((i)=>
+        {
+            if (i<10)
+                {return true;}
+            else
+                {return false;}
+        }));
+        Console.WriteLine(doubleArray.EvenIfOne((i) => { if (i < 0) { return true;} else { return false;} }));
+        Console.WriteLine(intArray.IfAll((i) => { if (i > 10) { return true; } else { return false; } }));
+        Console.WriteLine(doubleArray.IfAll((i) => { if (i > 0) { return true; } else { return false; } }));
+
+    }
+}
